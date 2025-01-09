3008a89c20447417cf6e8b40edb877cf02dfaf1b
Brz2521 <MACETs.J2911@gmail.com>
2025-01-08 23:57:10 -0500

    press key to change image

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const myBox = document.getElementsByClassName("icon-button");
-const otherContent = document.getElementsById("otherIcon");
+const myBox = document.getElementsByClassName("image1")[0];
+const image = document.createElement("img");
+image.src = "gift-image.png";
+const otherContent = document.createElement("div");
+otherContent.className = "image2";
+
+otherContent.appendChild(image);
+const originalImageSrc = myBox.querySelector("img").src;
+
 
 document.addEventListener("keydown", event => {
-    myBox.textContent = otherContent;
+    myBox.querySelector("img").src = otherContent.querySelector("img").src;
     myBox.style.backgroundColor = "tomato";
 });
 
 document.addEventListener("keyup", event => {
+    myBox.querySelector("img").src = originalImageSrc;
+    myBox.style.backgroundColor = "lightBlue";
 });
